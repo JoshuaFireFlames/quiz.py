f7cb2e9da6749e9cbf28b72a46a4102c8135945a
csPython <cspython@AD25TF04NGF1J-59.local>
2019-01-11 09:58:01 -0500

    Finished feedback based on score.

--- quizJW.py
+++ quizJW.py
@@ -148,5 +148,42 @@ run_quest(q10, q10Ans, answered10, q10r)
 
 #Score
 print("you got:", score * 10, "% on the quiz")
+if score == 10:
+    print("You did a great job.")
+    
+elif score == 9:
+    print("You were so close from a perfect score.")
+    
+elif score == 8:
+    print("You were close you should try again")
+    
+elif score == 7:
+    print("You should try again that wasn't a bad score")
+
+elif score == 6:
+    print("You should try again becuase I knwo you can get a much better score")
+
+elif score == 5:
+    print("You didn't get a great score were you trying.")
+
+elif score == 4:
+    print("You failed but I beleive you can get a way better score")
+
+elif score == 3:
+    print("You should try again becuase you have the potiential to do better")
+
+elif score == 2:
+    print("This is not a great score, you can do much better") 
+
+elif socre == 1:
+    print("Were you trying")
+
+elif socre == 0:
+    print("You should go on google to look up the answers and study to get a better score")
+
+else:
+    print("Ok you have made a mistake please read the questions throughly.")
+
+          
 
 
