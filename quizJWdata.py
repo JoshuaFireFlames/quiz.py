2aca0270ecb9b1d6f3702b7c1852a8fb01721b88
csPython <cspython@AD25TF04NGF1J-59.local>
2019-01-14 14:49:06 -0500

    Created data file.

--- quizJWdata.py
+++ quizJWdata.py
@@ -22,6 +22,109 @@ def run_quest(quest, ansU, check, ansR):
         except ValueError:
             print("Please read answer choices.") # Non-integer response
 
+#q1 variables
+q1 ="""How many letters are in the alphabet ?
+1)26
+2)6
+3)40
+4)22"""
+q1Ans = int(0)
+answered1 = False
+q1r = 1
+
+
+#q2 vaiables
+q2 ="""How many planets are in our solar system ?
+1)15
+2)10
+3)8
+4)7"""
+q2Ans = int(0)
+answered2 = False
+q2r = 3
+
+#q3 vaiables
+q3 ="""What is the fastest water animal ?
+1)Porpoise
+2)Sailfish
+3)Flying fish
+4)Tuna"""
+q3Ans = int(0)
+answered3 = False
+q3r = 2
+
+#q4 vaiables
+q4 ="""When was apple founded ? 
+1)1969
+2)1970
+3)2000
+4)1976"""
+q4Ans = int(0)
+answered4 = False
+q4r = 4
+
+#q5 vaiables
+q5 ="""Who invented the game of basketball ? 
+1)Adam Sliver
+2)William G. Morgan
+3)James Naismith
+4)Micheal Jordan"""
+q5Ans = int(0)
+answered5 = False
+q5r = 3
+
+#q6 vaiables
+q6 ="""Who created the periodic table ? 
+1)Dmitri Mendeleev
+2)John Newlands
+3)Henry Moseley
+4)J.J Thomson"""
+q6Ans = int(0)
+answered6 = False
+q6r = 1
+
+#q7 vaiables
+q7 ="""Which of the following country is the coldest in the world ? 
+1)Canda
+2)Antartica
+3)Russia
+4)Greenland"""
+q7Ans = int(0)
+answered7 = False
+q7r = 2
+
+#q8 vaiables
+q8 ="""How much is fornite worth ? 
+1)$20 billion
+2)$15 billion
+3)$50 billion
+4)$8 billion"""
+q8Ans = int(0)
+answered8 = False
+q8r = 4
+
+#q9 vaiables
+q9 ="""How tall is Mount Everst ? 
+1)20,150ft
+2)24,400ft
+3)20,029ft
+4)25,900ft"""
+q9Ans = int(0)
+answered9 = False
+q9r = 3
+  
+#q10 vaiables
+q10 ="""When was the aston martin (cars) founded ? 
+1)1913
+2)1989
+3)1907
+4)1932"""
+q10Ans = int(0)
+answered10 = False
+q10r = 1 
+  
+  
+  
 
 #intro
 print ("Hello and welcome to my ten question quiz")
