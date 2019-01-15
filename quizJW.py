79810b84358e96e9016891a4a0150db4301b94ba
csPython <cspython@AD25TF04NGF1J-59.local>
2019-01-15 11:56:09 -0500

    Program file imports data file (Changed lead comment).

--- quizJW.py
+++ quizJW.py
@@ -1,29 +1,11 @@
-#Importing data file
+#The program importing data and feedback based on score.
 
+#Importing the data.
 from quizJWdata import *
 
+# Score
 global score
 score = 0 
-
-def run_quest(quest, ansU, check, ansR):
-    print(quest)
-    while check == False:
-        try:
-            ansU = int(input("Choose the best reponse that seems fit." ))
-            if ansU == ansR:
-                print("Ok")
-                global score 
-                score += 1
-                check = True
-            elif 0 < ansU < 5:
-                print("Ok")
-                check = True
-            else:
-                print("Please enter a integer 1-4.") # unacceptable int response
-
-        except ValueError:
-            print("Please read answer choices.") # Non-integer response
-
 
 #intro
 print ("Hello and welcome to my ten question quiz")
@@ -31,7 +13,6 @@ print ("In this program, you will be asked one question and given fourm options.
 print ("To chose from.")
 print ("When you are ready to answer the question, hit enter.")
 #These are the intructions above.
-
 
 # run questions 1-10
 run_quest(q1, q1Ans, answered1, q1r)
@@ -45,7 +26,7 @@ run_quest(q8, q8Ans, answered8, q8r)
 run_quest(q9, q9Ans, answered9, q9r)
 run_quest(q10, q10Ans, answered10, q10r)
 
-#Score
+#Score and feedback based on score 
 print("you got:", score * 10, "% on the quiz")
 if score == 10:
     print("You did a great job.")
