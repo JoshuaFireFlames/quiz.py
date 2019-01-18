b757e1c831f8e05fb65a77b4d373cbee125e4e9b
csPython <cspython@AD25TF04NGF1J-59.local>
2019-01-18 16:20:32 -0500

    Data file with global score and fully commented.

--- quizJWdata.py
+++ quizJWdata.py
@@ -5,14 +5,14 @@ global score
 score = 0
 
 #q1 variables
-q1 ="""How many letters are in the alphabet ?
+q1 ="""How many letters are in the alphabet ? 
 1)26
 2)6
 3)40
 4)22"""
-q1Ans = int(0)
-answered1 = False
-q1r = 1
+q1Ans = int(0) #Keep the user in the loop until a acceptable answer.
+answered1 = False #The users answer.
+q1r = 1 #The correct answer.
 
 
 #q2 vaiables
@@ -21,9 +21,9 @@ q2 ="""How many planets are in our solar system ?
 2)10
 3)8
 4)7"""
-q2Ans = int(0)
-answered2 = False
-q2r = 3
+q2Ans = int(0) #Keep the user in the loop until a acceptable answer.
+answered2 = False #The users answer.
+q2r = 3 #The correct answer.
 
 #q3 vaiables
 q3 ="""What is the fastest water animal ?
@@ -31,9 +31,9 @@ q3 ="""What is the fastest water animal ?
 2)Sailfish
 3)Flying fish
 4)Tuna"""
-q3Ans = int(0)
-answered3 = False
-q3r = 2
+q3Ans = int(0) #Keep the user in the loop until a acceptable answer.
+answered3 = False #The users answer.
+q3r = 2 #The correct answer.
 
 #q4 vaiables
 q4 ="""When was apple founded ? 
@@ -41,9 +41,9 @@ q4 ="""When was apple founded ?
 2)1970
 3)2000
 4)1976"""
-q4Ans = int(0)
-answered4 = False
-q4r = 4
+q4Ans = int(0) #Keep the user in the loop until a acceptable answer.
+answered4 = False #The users answer.
+q4r = 4 #The correct answer.
 
 #q5 vaiables
 q5 ="""Who invented the game of basketball ? 
@@ -51,9 +51,9 @@ q5 ="""Who invented the game of basketball ?
 2)William G. Morgan
 3)James Naismith
 4)Micheal Jordan"""
-q5Ans = int(0)
-answered5 = False
-q5r = 3
+q5Ans = int(0) #keep the user in the loop until a acceptable answer.
+answered5 = False #the users answer.
+q5r = 3 #The correct answer.
 
 #q6 vaiables
 q6 ="""Who created the periodic table ? 
@@ -61,9 +61,9 @@ q6 ="""Who created the periodic table ?
 2)John Newlands
 3)Henry Moseley
 4)J.J Thomson"""
-q6Ans = int(0)
-answered6 = False
-q6r = 1
+q6Ans = int(0) #keep the user in the loop until a acceptable answer.
+answered6 = False #the users answer.
+q6r = 1 #The correct answer.
 
 #q7 vaiables
 q7 ="""Which of the following country is the coldest in the world ? 
@@ -71,9 +71,9 @@ q7 ="""Which of the following country is the coldest in the world ?
 2)Antartica
 3)Russia
 4)Greenland"""
-q7Ans = int(0)
-answered7 = False
-q7r = 2
+q7Ans = int(0) #Keep the user in the loop until a acceptable answer.
+answered7 = False #The users answer.
+q7r = 2 #The correct answer.
 
 #q8 vaiables
 q8 ="""How much is fornite worth ? 
@@ -81,9 +81,9 @@ q8 ="""How much is fornite worth ?
 2)$15 billion
 3)$50 billion
 4)$8 billion"""
-q8Ans = int(0)
-answered8 = False
-q8r = 4
+q8Ans = int(0) #Keep the user in the loop until a acceptable answer.
+answered8 = False #The users answer.
+q8r = 4 #The correct answer.
 
 #q9 vaiables
 q9 ="""How tall is Mount Everst ? 
@@ -91,9 +91,9 @@ q9 ="""How tall is Mount Everst ?
 2)24,400ft
 3)20,029ft
 4)25,900ft"""
-q9Ans = int(0)
-answered9 = False
-q9r = 3
+q9Ans = int(0) #Keep the user in the loop until a acceptable answer.
+answered9 = False #The users answer.
+q9r = 3 #The correct answer.
   
 #q10 vaiables
 q10 ="""When was the aston martin (cars) founded ? 
@@ -101,6 +101,6 @@ q10 ="""When was the aston martin (cars) founded ?
 2)1989
 3)1907
 4)1932"""
-q10Ans = int(0)
-answered10 = False
-q10r = 1 
+q10Ans = int(0) #Keep the user in the loop until a acceptable answer.
+answered10 = False #The users answer.
+q10r = 1 #The correct answer.
